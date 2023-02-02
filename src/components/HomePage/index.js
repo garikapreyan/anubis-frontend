2676d975e0e361585e48ccce29283e3484808b2d
Garik Apreyan <garik.apreyan@gmail.com>
2023-02-02 20:24:41 +0400

    [edit] eslint rules and [fix] eslint problems

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function HomePage (){
+function HomePage() {
   return (
     <p style={{marginTop: '100px'}}>
       Home page
     </p>
-  )
+  );
 }
 
 export default HomePage;
